2069fd67c4ef818800c95a46ddec6e862376a9d3
Aivaxplus <sabri.makhoukhi@gmail.com>
2025-10-30 12:13:18 +0100

    Initial Commit

---
+++ scrolldeeds/scrolldeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrolldeedsApp.swift
+//  scrolldeeds
+//
+//  Created by Sabri El makhoukhi on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct scrolldeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
